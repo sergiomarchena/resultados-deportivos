7e4587f3832fc1013a913ab275fc0b47719d29b5
Sergio Marchena Quiros <ortonchico_sergio@hotmail.com>
2013-03-06 09:42:08 +0100

    programa python consultas a la api

---
+++ aplicacion.py
@@ -0,0 +1,11 @@
+import requests
+
+resultados = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params = {"key":"2b42a02ecec6a92d100a0b1b5e3a6e4d", "format":"xml", "req":"matchs", "league":"1"}) 
+
+clasificacion = requests.get ("http://www.resultados-futbol.com/scripts/api/api.php",  params = {"key":"2b42a02ecec6a92d100a0b1b5e3a6e4d", "format":"xml", "req":"tables", "league":"1"})
+
+
+if resultados.status_code == 200 and clasificacion.status_code == 200:
+
+    print resultados.text
+    print clasificacion.text
