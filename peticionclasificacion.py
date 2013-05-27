d7825a8b908cff63ee061220f49b994bb1eba4c8
Sergio Marchena Quiros <ortonchico_sergio@hotmail.com>
2013-05-27 13:40:15 +0200

    borrando lo que sobra

--- peticionclasificacion.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-clasificacion = requests.get("http://www.resultados-futbol.com/scripts/api/api.php",  params = {"key":"2b42a02ecec6a92d100a0b1b5e3a6e4d", "format":"xml", "req":"tables", "league":"1"}) 
-
-
-if clasificacion.status_code == 200:
-
-    print clasificacion.text
